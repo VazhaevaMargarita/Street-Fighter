5267549178c3496f477abd47b97582a1b9f506a0
VazhaevaMargarita <vazhaevamargarita@gmail.com>
2026-04-24 19:38:47 +0300

    sdhdfsh

--- StreetFighter/Assets/Scripts/EnemyController.cs
+++ StreetFighter/Assets/Scripts/EnemyController.cs
@@ -6,14 +6,48 @@ public class EnemyController : MonoBehaviour
     public int _health = 100;
     [SerializeField] private  PlayerMovement _playerMovement;
     private Rigidbody2D _rb;
+    [SerializeField] SpriteRenderer _sre;
+    private Animator _animatore;
+    public event Action HitEnemyEvent;
+
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animatore = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        _rb.linearVelocity = new Vector2(_playerMovement.transform.position.x, _playerMovement.transform.position.y );
+        
+        
+        float sqrDistance = (transform.position - _playerMovement.transform.position).sqrMagnitude;
+
+        if (sqrDistance < 4f)
+        {
+            _animatore.SetFloat("Move", 0f); 
+            _animatore.SetBool("Hit", true); 
+        }
+        else
+        {
+            Vector3 direction = (_playerMovement.transform.position - transform.position).normalized;
+
+            _rb.linearVelocity = direction * 5f;
+            _animatore.SetFloat("Move", 1f); 
+            _animatore.SetBool("Hit", false); 
+        }
+        
     }
+    
+    public void HitEnemy()
+    {
+        
+        float sqrDistance = (transform.position - _playerMovement.transform.position).sqrMagnitude;
+
+        if (sqrDistance < 3f)
+        {
+            HitEnemyEvent?.Invoke();
+        }
+    }
+    
 }
--- StreetFighter/Assets/Scripts/MainManager.cs
+++ StreetFighter/Assets/Scripts/MainManager.cs
@@ -8,21 +8,33 @@ public class MainManager : MonoBehaviour
     [SerializeField] private EnemyController _enemyController ;
     
     [SerializeField] private  PlayerAtack _playerAtack;
-    
+    [SerializeField] private Animator _playerAnimator;
+    [SerializeField] private Animator _enemyAnimator;
     private void OnEnable()
     {
         _playerAtack.HitPlayerEvent += PlayerHealth;
+        _enemyController.HitEnemyEvent += EnemyHealth;
     }
 
     private void PlayerHealth()
     {
         _enemyController._health = _health.CalcHealth(_setitngs._damage,_enemyController._health);
-        Debug.Log(_enemyController._health);
+        Debug.Log("EnemyHp="+_enemyController._health);
+        _enemyAnimator.SetBool("Hurt", true); 
+        
+    }
+    
+    private void EnemyHealth()
+    {
+        _playerAtack._health = _health.CalcHealth(_setitngs._damage,_playerAtack._health);
+        Debug.Log("PlayerHp="+_playerAtack._health);
+        _playerAnimator.SetBool("Hurt", true);
         
     }
 
     private void OnDisable()
     {
         _playerAtack.HitPlayerEvent -= PlayerHealth;
+        _enemyController.HitEnemyEvent -= EnemyHealth;
     }
 }
